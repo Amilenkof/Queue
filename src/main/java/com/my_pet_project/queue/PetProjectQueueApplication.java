0d307da70faa7d96022aee6effc66f3ae279ab18
Amilenkof <f.o.g@list.ru>
2023-06-05 19:41:42 +0400

    init commit

---
+++ src/main/java/com/my_pet_project/queue/PetProjectQueueApplication.java
@@ -0,0 +1,13 @@
+package com.my_pet_project.queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetProjectQueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetProjectQueueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/my_pet_project/queue/PetProjectQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_pet_project.queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProjectQueueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
